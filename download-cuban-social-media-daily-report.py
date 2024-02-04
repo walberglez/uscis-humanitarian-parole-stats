5cae807a9cbb673f84739ac9356d62c1b36d786c
walberglez <walber.glez@gmail.com>
2024-02-04 11:29:17 -0500

    new url

--- download-cuban-social-media-daily-report.py
+++ download-cuban-social-media-daily-report.py
@@ -21,7 +21,8 @@ CUBAN_SOCIAL_MEDIA_DAILY_REPORT_BASE_URL = [
   'https://notiparole.com/paroles-aprobados-{date}-casos-cuba/',
   'https://notiparole.com/paroles-aprobados-{date}-de-cubanos/',
   'https://notiparole.com/paroles-aprobados-{date}-cuba-cubanos/',
-  'https://notiparole.com/lista-paroles-aprobados-{date}-cuba/'
+  'https://notiparole.com/lista-paroles-aprobados-{date}-cuba/',
+  'https://notiparole.com/cubanos-con-paroles-aprobados-{date}/'
 ]
 SPANISH_MONTH_NAMES = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
 SPANISH_MONTH_ABBR = ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic']
